7095aa6eebd35056771d2d91155318e8cdad11db
Julian Ordaz <125109952+jordaz14@users.noreply.github.com>
2024-04-17 18:42:25 -0400

    Validate user input as email

--- script.js
+++ script.js
@@ -4,12 +4,33 @@ let formInput = document.querySelector("#form-email-input");
 
 formButton.addEventListener("click", (e) => {
   e.preventDefault();
-  let params = {
-    from_name: "Too Serious",
-    email_id: formInput.value,
-  };
-  emailjs.send("service_3spwn2m", "template_115bpeu", params).then(() => {
-    formPrompt.textContent = `welcome to my thoughts`;
+
+  // If valid email, perform send email to user
+  if (validateEmail(formInput)) {
+    let params = {
+      from_name: "Too Serious",
+      email_id: formInput.value,
+    };
+    emailjs.send("service_3spwn2m", "template_115bpeu", params).then(() => {
+      formPrompt.textContent = `welcome to my thoughts`;
+      formInput.value = "";
+    });
+  }
+  // Else, prompt user to try again
+  else {
+    formPrompt.textContent = `oops... try again`;
     formInput.value = "";
-  });
+  }
 });
+
+// Validate user input as email
+function validateEmail(input) {
+  let validRegex =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+  if (input.value.match(validRegex)) {
+    return true;
+  } else {
+    return false;
+  }
+}
