01b5775fb40b1f252853ccbb6a6a2a07cf2be464
Julian Ordaz <125109952+jordaz14@users.noreply.github.com>
2024-04-16 22:02:08 -0400

    Send form entry to user email

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 let formPrompt = document.querySelector("#form-prompt");
 let formButton = document.querySelector("form button");
+let formInput = document.querySelector("#form-email-input");
 
 formButton.addEventListener("click", (e) => {
   e.preventDefault();
-  console.log("hi");
-  formPrompt.textContent = "oops... try again later";
+  let params = {
+    from_name: "Too Serious",
+    email_id: formInput.value,
+  };
+  emailjs.send("service_3spwn2m", "template_115bpeu", params).then(() => {
+    formPrompt.textContent = `Welcome ${formInput.value}.`;
+    formInput.value = "";
+  });
 });
